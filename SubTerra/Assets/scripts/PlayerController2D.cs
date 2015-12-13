5740194e159c6b83af4ca606f5b80cfd4e569752
gregclarke <clarke.gregk@gmail.com>
2015-12-13 19:03:59 +1100

    Clean up PlayerController2D

--- SubTerra/Assets/scripts/PlayerController2D.cs
+++ SubTerra/Assets/scripts/PlayerController2D.cs
@@ -14,7 +14,6 @@ public class PlayerController2D : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		//rb.AddForce(new Vector2 (speed, 0));
 		rb.velocity = new Vector2 (speed, 0);
 	}
 	
@@ -22,18 +21,11 @@ public class PlayerController2D : MonoBehaviour {
 	void Update () {
 
 		if (Input.GetMouseButtonDown (0)) {
-			//velocity = new Vector2 (rigidbody.velocity.x, jumpVelocity);
 			jump = true;
 		}
-
-		///float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-		//float moveVertical = Input.GetAxisRaw ("Vertical");
-
-		//velocity = new Vector2 (moveHorizontal, moveVertical).normalized *10;
 	}
 
 	void FixedUpdate() {
-		//Vector2 newPosition = rigidbody.position + velocity * Time.fixedDeltaTime;
 		if (jump) {
 			rb.AddForce (new Vector2 (0, jumpForce));
 			jump = false;
